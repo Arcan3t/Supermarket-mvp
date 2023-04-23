6962ff4f1dfc2d0d8e2519d954cec499db0f4c09
Juan Pablo Acosta <pabloacosta_999@hotmail.com>
2023-04-22 20:19:56 -0500

    ProviderRepository

--- Models/ProviderModel.cs
+++ Models/ProviderModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,12 @@ namespace Supermarket_mvp.Models
 {
     internal class ProviderModel
     {
+        [DisplayName("Provider Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Provider Name")]
+        [Required(ErrorMessage = "Provider name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Provider name must be between 3 and 50 characters")]
+        public string Name { get; set; }
     }
 }
